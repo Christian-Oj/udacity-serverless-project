70d0b291b912ba46989762669149923ab76dfdce
Christian Ojiezele <christianojiezele@gmail.com>
2022-11-06 22:30:15 +0000

    updated editTodo

--- client/src/components/EditTodo.tsx
+++ client/src/components/EditTodo.tsx
@@ -11,7 +11,7 @@ enum UploadState {
 
 interface EditTodoProps {
   match: {
-    paramPayload: {
+    params: {
       todoId: string
     }
   }
@@ -51,7 +51,7 @@ export class EditTodo extends React.PureComponent<
       }
 
       this.setUploadState(UploadState.FetchingPresignedUrl)
-      const uploadUrl = await getUploadUrl(this.props.auth.getIdToken(), this.props.match.paramPayload.todoId)
+      const uploadUrl = await getUploadUrl(this.props.auth.getIdToken(), this.props.match.params.todoId)
 
       this.setUploadState(UploadState.UploadingFile)
       await uploadFile(uploadUrl, this.state.file)
